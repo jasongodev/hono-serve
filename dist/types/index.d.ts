2419aac586f07eafba5b31a40fe21bc68dfc6479
Jason Go <jasongo@jasongo.net>
2023-03-12 06:50:10 +0800

    edit generics

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,4 +1,4 @@
-import { Hono } from 'hono';
+import { Hono, Env } from 'hono';
 export declare function getRuntime(): string;
 export interface HonoServeOptions {
     bun?: {
@@ -21,4 +21,4 @@ export type HonoFastlyReturn = Hono;
 export type HonoNextjsReturn = (req: Request) => Response | Promise<Response>;
 export type HonoVercelReturn = (req: Request, res: Response) => Response | Promise<Response>;
 export type HonoServe = HonoBunReturn | HonoCloudflareReturn | HonoFastlyReturn | HonoNextjsReturn | HonoVercelReturn | Hono;
-export declare const serve: (app: Hono, options?: HonoServeOptions) => HonoServe;
+export declare const serve: <E extends Env>(app: Hono<E, {}, "">, options?: HonoServeOptions) => HonoServe;
